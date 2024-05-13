0a72169cbdb793c35c0bb0e641765516a0000668
saii340 <111050937+saii340@users.noreply.github.com>
2024-05-13 13:40:58 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "";
+$server = "database-1.cbkuqme8ee0s.ap-southeast-2.rds.amazonaws.com";
+$username = "admin";
+$password = "Bitcotphp";
 $db = "login";
 
 $conn = new mysqli($server, $username, $password, $db);
